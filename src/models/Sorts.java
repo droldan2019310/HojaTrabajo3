e390ded01d51561d1b621d595a138ced5f435dc1
PabloAlvarez22 <pabloalv567@gmail.com>
2023-02-08 15:55:42 -0600

    arreglos

--- src/models/Sorts.java
+++ src/models/Sorts.java
@@ -4,22 +4,4 @@ import java.util.ArrayList;
 
 public class Sorts<T extends Comparable<T>>  {
 
-
- public T[] bubbleSort(T[] array) {
-
- for (int i = array.length - 1; i > 0; i--) {
-		for (int j = 0; j < i; j++) {
-			if (array[j].compareTo(array[j + 1])==1)  {
-				swap(array, j, j+1);
-			}
-		}
-	}
- return array;
-}
-
-public void swap(T[] array, int a, int b) {
-	T value = array[b];
-	array[b] = array[a];
-	array[a] = value;
-}
 }
